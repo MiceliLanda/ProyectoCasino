72d662ce74dcb8b37c34357e23c3503cb8187725
EduDev011098 <183423@ids.upchiapas.edu.mx>
2020-10-09 23:53:54 -0500

    Socketsv1

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,4 +1,36 @@
 package model;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Cliente {
+
+    public static void main(String [] args){
+
+        final String Host ="127.0.0.1";
+        final int puerto = 5000;
+        DataInputStream in;
+        DataOutputStream out;
+        Socket sc;
+
+        {
+            try {
+                sc = new Socket(Host, puerto);
+                in = new DataInputStream(sc.getInputStream());
+                out = new DataOutputStream(sc.getOutputStream());
+
+                out.writeUTF("soy el cliente");
+                String mensaje = in.readUTF();
+                System.out.println("mensaje");
+                sc.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
 }
--- src/model/Servidor.java
+++ src/model/Servidor.java
@@ -1,4 +1,50 @@
 package model;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
 public class Servidor {
+
+    public static void main(String [] args){
+
+        Socket sc = null;
+        final int puerto = 5000;
+        ServerSocket server;
+        DataInputStream in;
+        DataOutputStream out;
+
+        {
+            try {
+                server = new ServerSocket(puerto);
+                System.out.println("Servioor encendido");
+
+                while(true){
+                    sc = server.accept();
+                    System.out.println("Client Connected");
+                    in = new DataInputStream(sc.getInputStream());
+                    out = new DataOutputStream(sc.getOutputStream());
+
+                    String mensaje = in.readUTF();
+                    System.out.println(mensaje);
+
+                    out.writeUTF("Mensaje del servidor");
+
+                    sc.close();
+                    System.out.println("Client Disconnected");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+
+
+
 }
